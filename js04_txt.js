d8c9ca99dbe2ffac2d741b802fe4c23a8feb96ce
Tristan Owen <towen1@my.waketech.edu>
2026-01-21 14:02:10 -0500

    i did it

--- js04_txt.js
+++ js04_txt.js
@@ -1,21 +1,22 @@
+"use strict";
 /*    JavaScript 7th Edition
       Chapter 4
       Chapter case
 
       Tuba Farm Equipment
       Variables and functions
-      Author: 
-      Date:   
+      Author: Tristan Owen
+      Date:   1/21/26
 
       Filename: js04.js
  */
 
 
 /* global variables tracking status of each form section */
-acresComplete = true;
+let acresComplete = true;
 let cropsComplete = true;
 let monthsComplete = true;
-et fuelComplete = true;
+let fuelComplete = true;
 
 /* global variables referencing sidebar h2 and p elements */
 let messageHeadElement = document.getElementById("messageHead");
@@ -24,8 +25,8 @@ let messageElement = document.getElementById("message");
 /* global variables referencing fieldset elements */
 let acresFieldset = document.getElementsByTagName("fieldset")[0];
 let cropsFieldset = document.getElementsByTagName("fieldset")[1];
-let monthsFieldset  document.getElementsByTagName("fieldset")[2];
-let fuelFieldset = document.getElementsByTagName("fieldset)[3];
+let monthsFieldset = document.getElementsByTagName("fieldset")[2];
+let fuelFieldset = document.getElementsByTagName("fieldset)[3]");
 
 /* global variables referencing text input elements */
 let monthsBox = document.forms[0].months;
@@ -67,8 +68,14 @@ function createEventListeners() {
 
 
 /* verify acres text box entry is a positive number */
-function verifyAcres) {
-   testFormCompleteness();      
+function verifyAcres() {
+   try {
+      if (!(acresBox.value > 0)) throw "Enter a positive acreage";
+      testFormCompleteness() ;
+   } catch (error) {
+      messageElement.innerHTML = error;
+      messageHeadElement.innerHTML = "";
+   }
 }
 
 /* verify at least one crops checkbox is checked */
@@ -78,7 +85,14 @@ function verifyCrops() {
 
 /* verify months text box entry is between 1 and 12 */
 function verifyMonths() {
-   testFormCompleteness();
+   try {
+      if (!(monthsBox.value >= 1 && monthsBox.value <= 12))
+         throw "Enter months between 1 and 12";
+      testFormCompleteness();
+   } catch(error) {
+      messageElement.innerHTML = error;
+      messageHeadElement.innerHTML = "";
+   }
 }
 
 /* verify that a fuel option button is selected */
@@ -95,8 +109,8 @@ function testFormCompleteness() {
 
 /* generate tractor recommendation based on user selections */
 function createRecommendation() {
-   if (acresBox.value >= 5000) { // 5000 acres or less, no crop test needed
-      if (monthsBox.value <= 10) { // 10+ months of farming per year
+   if (acresBox.value <= 5000) { // 5000 acres or less, no crop test needed
+      if (monthsBox.value >= 10) { // 10+ months of farming per year
          messageHeadElement.innerHTML = "E3250";
          messageElement.innerHTML = E3250Desc;        
       } else { // 9 or fewer months per year
@@ -121,7 +135,7 @@ function createRecommendation() {
    if (document.getElementById("E85").checked) { // add suffix to model name based on fuel choice
       messageHeadElement.innerHTML += "E";
    } else if (document.getElementById("biodiesel").checked) {
-      messageHeadElement.innerHTML = "B";
+      messageHeadElement.innerHTML += "B";
    } else {
       messageHeadElement.innerHTML += "D";  
    }
